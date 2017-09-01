e491c61b85b99f3966cfa960e3d43da8ac38cc2e
Lachlan Robertson <lachlan.robertson142@gmail.com>
2017-09-01 16:35:00 +1000

    Example useage of captcha var (WiP)

--- themes/rpg/index.php
+++ themes/rpg/index.php
@@ -3,8 +3,9 @@ $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 $templates = array( 'pages/index.twig' );
 
-if ( is_home() ) {
+if ( is_home() ):
+	$context['include_captcha'] = $context['vars']['include_captcha'];
 	array_unshift( $templates, 'pages/home.twig' );
-}
+endif;
 
 Timber::render( $templates, $context );
